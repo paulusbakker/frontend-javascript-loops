49f018f3249295ba77ddc671e9eaefa6717737a7
paulusbakker <paulusbakker@gmail.com>
2021-06-28 21:57:41 +0200

    allesklar

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -5,10 +5,10 @@
 // ==========================================
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
-
-for (let i = 0, names.length -1, i++) {
-    console.log(names[i])
+for (let i = 0; i < names.length; i++) {
+    names[i]= names[i] + "je"
 }
+console.log(names)
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -24,7 +24,10 @@ for (let i = 0, names.length -1, i++) {
 // ==========================================
 
 const numbers = [2, 4, 5, 29, 38];
-
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i]= numbers[i] * 3
+}
+console.log(numbers)
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
@@ -40,7 +43,10 @@ const numbers = [2, 4, 5, 29, 38];
 // ==========================================
 
 const squares = [30, 2, 8, 24, 11];
-
+for (let i = 0; i < squares.length; i++) {
+    squares[i]= squares[i] ** 3
+}
+console.log(squares)
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [30, 2, 8, 24, 11];
